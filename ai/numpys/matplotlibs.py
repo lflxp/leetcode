592b30af8ded695522c91474c6d9f1e5aa9df427
yckj0834 <lixueping@cloudwalk.cn>
2019-09-03 16:59:00 +0800

    no common

--- ai/numpys/matplotlibs.py
+++ ai/numpys/matplotlibs.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*
+#!/usr/bin/env python3
 import numpy as np 
 from matplotlib import pyplot as plt 
  
--- m2.go
+++ m2.go
@@ -1,34 +1,38 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"log"
-	"os"
+	"fmt"
 )
 
 func main() {
 
-	type Road struct {
-		Name   string
-		Number int
-	}
-	roads := []Road{
-		{"Diamond Fork", 29},
-		{"Sheep Creek", 51},
-	}
+	// type Road struct {
+	// 	Name   string
+	// 	Number int
+	// }
+	// roads := []Road{
+	// 	{"Diamond Fork", 29},
+	// 	{"Sheep Creek", 51},
+	// }
 
-	b, err := json.Marshal(roads)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// b, err := json.Marshal(roads)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 
-	var out bytes.Buffer
-	err = json.Indent(&out, b, "", "\t")
+	// var out bytes.Buffer
+	// err = json.Indent(&out, b, "", "\t")
 
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 
-	out.WriteTo(os.Stdout)
+	// out.WriteTo(os.Stdout)
+
+	// a := "abc (123)"
+	// a := "abc 998"
+	// fmt.Println(strings.Split(a, "(")[0])
+
+	a := 123.123
+	fmt.Println(fmt.Sprintf("%d", int(a)))
 }
