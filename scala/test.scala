9c9e8123d935c9f21a92c6a86ef7aab7e31041b2
lflxp <lxp@lflxp.cn>
2019-11-21 01:08:11 +0800

    add scala

---
+++ scala/test.scala
@@ -0,0 +1,11 @@
+val s="hello world"
+println(s)
+
+val data = List(3,4,2,1,5,"hello")
+for (s<-data) println(s)
+
+val info = Array(2,3,4,5,6,7,7,8,89,1)
+for (ss<-info) println(ss)
+
+println("######")
+info.foreach(println)
